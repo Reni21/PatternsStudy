e5db2922a4df468e017930598a0355eb2170ac01
Reni21 <r.phoenixart@gmail.com>
2019-11-03 02:19:29 +0200

    Init commit

---
+++ src/main/java/com/reni/task03/ArticleApp.java
@@ -0,0 +1,34 @@
+package com.reni.task03;
+
+import com.reni.task03.entity.Article;
+import com.reni.task03.entity.Wikipedia;
+
+public class ArticleApp {
+    private static Wikipedia wikipedia = new Wikipedia();
+
+    public static void main(String[] args) {
+        Article articleOne = new Article( 1,"OOP", "Article one content");
+        wikipedia.addArticle(articleOne);
+        try {
+            wikipedia.addArticle(new Article(2,"OOP", "Article two content"));
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
+        Article articleTwo = new Article(2,"JAVA", "Article two content");
+        wikipedia.addArticle(articleTwo);
+        Article articleCopy = wikipedia.getArticleById(2);
+
+        System.out.println(articleCopy.equals(articleTwo)); // true
+        System.out.println(articleCopy == articleTwo); // false
+
+        articleCopy.setContent("New content for copy of articleTwo");
+        wikipedia.updateArticle(articleCopy);
+
+        System.out.println(articleCopy.getContent().equals(articleTwo.getContent())); // true
+
+        wikipedia.resetLastChangesInArticleById(2);
+
+        System.out.println(articleCopy.getContent().equals(articleTwo.getContent())); // false
+    }
+}
---
+++ src/main/java/com/reni/task03/entity/Article.java
@@ -0,0 +1,67 @@
+package com.reni.task03.entity;
+
+public class Article implements Copyable {
+    private int id;
+    private String title;
+    private String content;
+
+    public Article(int id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public Article copy() {
+        return new Article(id, title, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article)) return false;
+
+        Article article = (Article) o;
+
+        if (getId() != article.getId()) return false;
+        if (getTitle() != null ? !getTitle().equals(article.getTitle()) : article.getTitle() != null) return false;
+        return getContent() != null ? getContent().equals(article.getContent()) : article.getContent() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId();
+        result = 31 * result + (getTitle() != null ? getTitle().hashCode() : 0);
+        result = 31 * result + (getContent() != null ? getContent().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
